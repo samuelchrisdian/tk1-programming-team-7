fda8f5650f2a57a77ccc1b8df1d3698a88363fb9
Putra A <windslash31@gmail.com>
2022-09-16 21:55:49 +0700

    - nambah while loop untuk porsi dibawah 0 atau diatas 10 - menunggu input enter untuk exit = rapiin harga dikit

--- src/Restoran.java
+++ src/Restoran.java
@@ -34,16 +34,40 @@ public class Restoran {
         System.out.println("\nPesanan Anda [batas pesanan 0-10 porsi]");
         System.out.print("1. Nasi Goreng Spesial    = ");
         pesan1 = keyboard.nextInt();
+
+        //while loop jika pesanan dibawah 0 atau 10 porsi
+        while (pesan1 < 0 || pesan1 > 10){
+            System.out.print("Tolong Masukan Jumlah Porsi Yang Sesuai: ");
+            pesan1 = keyboard.nextInt();
+        }
+
         System.out.print("2. Ayam Bakar Spesial     = ");
         pesan2 = keyboard.nextInt();
+        while (pesan2 < 0 || pesan2 > 10){
+            System.out.print("Tolong Masukan Jumlah Porsi Yang Sesuai: ");
+            pesan2 = keyboard.nextInt();
+        }
         System.out.print("3. Steak Sirloin Spesial  = ");
         pesan3 = keyboard.nextInt();
+        while (pesan3 < 0 || pesan3 > 10){
+            System.out.print("Tolong Masukan Jumlah Porsi Yang Sesuai: ");
+            pesan3 = keyboard.nextInt();
+        }
         System.out.print("4. Kwetiaw Siram Spesial  = ");
         pesan4 = keyboard.nextInt();
+        while (pesan4 < 0 || pesan4 > 10){
+            System.out.print("Tolong Masukan Jumlah Porsi Yang Sesuai: ");
+            pesan4 = keyboard.nextInt();
+        }
         System.out.print("5. Kambing Guling Spesial = ");
         pesan5 = keyboard.nextInt();
-        
-        System.out.println("\nSelamat menikmati makanan anda...");
+        while (pesan5 < 0 || pesan5 > 10){
+            System.out.print("Tolong Masukan Jumlah Porsi Yang Sesuai: ");
+            pesan5 = keyboard.nextInt();
+        }
+
+        System.out.printf("\nSelamat menikmati makanan anda %s...\n",nama); //nambah string nama dibelakang dari input user
+
         System.out.println("\nPembelian:");
         System.out.println("1. Nasi Goreng Spesial      " + pesan1 + " porsi * Rp. 9999.99   = Rp. " + koma.format(pesan1*h1));
         System.out.println("2. Ayam Bakar Spesial       " + pesan2 + " porsi * Rp. 12345.67  = Rp. " + koma.format(pesan2*h2));
@@ -53,8 +77,8 @@ public class Restoran {
         System.out.println("================================================================== +");
         
         total = pesan1*h1 + pesan2*h2 + pesan3*h3 + pesan4*h4 + pesan5*h5;
-        System.out.println("Total pembelian                                     = Rp. " + total);
-        
+        System.out.printf("Total pembelian                                     = Rp.%10.2f",total); //biar rapi dikit
+        System.out.println("");
         diskon = total*10/100;
         System.out.println("Disc. 10% < Masa Promosi >                          = Rp. " + koma.format(diskon));
         System.out.println("================================================================== -");
@@ -63,10 +87,11 @@ public class Restoran {
         System.out.println("Total pembelian setelah diskon 10%                  = Rp. " + koma.format(bayar));
         
         perorang = bayar/jumlah;
-        System.out.println("Pembelian per orang <untuk " + jumlah + " orang>                  Rp. " + koma.format(perorang));
+        System.out.println("Pembelian per orang <untuk " + jumlah + " orang>                   Rp. " + koma.format(perorang));
         
         System.out.println("\nTerima kasih atas kunjungan Anda...");
         System.out.println("...tekan ENTER untuk keluar...");
-        
+        keyboard.nextLine();
+        keyboard.nextLine(); //Menunggu user menekan enter untuk exit
     }
 }
